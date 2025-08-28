748a2c0abc0947d4cde6e5d07c1c9c7ef19da423
Leon van Wissen <l.vanwissen@uva.nl>
2025-08-28 16:41:50 +0200

    update manifest

--- scripts/make_manifest.py
+++ scripts/make_manifest.py
@@ -3,20 +3,64 @@ import pandas as pd
 import iiif_prezi3
 
 iiif_prezi3.config.configs["helpers.auto_fields.AutoLang"].auto_lang = "en"
-iiif_prezi3.load_bundled_extensions()
 
 PREFIX = "https://globalise-huygens.github.io/document-view-sandbox/iiif/"
 
 
-def make_manifest(df):
-
+def make_manifest(df, navdate):
     manifest = iiif_prezi3.Manifest(
         id=f"{PREFIX}manifest.json",
         label="Demo Manifest for the GLOBALISE Document View Sandbox",
+        navDate=navdate,
+        navPlace=iiif_prezi3.NavPlace(
+            features=[
+                {
+                    "type": "Feature",
+                    "properties": {
+                        "name": "Makassar",
+                    },
+                    "geometry": {"type": "Point", "coordinates": [119.4055, -5.1339]},
+                }
+            ]
+        ),
+        rights="http://creativecommons.org/publicdomain/zero/1.0/",
+        requiredStatement=iiif_prezi3.KeyValueString(
+            label="Attribution",
+            value='<span>GLOBALISE Project. <a href="https://creativecommons.org/publicdomain/zero/1.0/"> <img src="https://licensebuttons.net/l/zero/1.0/88x31.png" alt="CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"/> </a> </span>',
+        ),
+    )
+
+    # Set logo and provider
+    globalise_logo = "https://globalise.huygens.knaw.nl/wp-content/uploads/2023/10/cropped-GLOBALISE_fuchsia-1200x630-1.png"
+
+    logo = iiif_prezi3.ResourceItem(
+        id=globalise_logo,
+        type="Image",
+        format="image/png",
+        height=630,
+        width=1200,
+    )
+
+    provider = iiif_prezi3.ProviderItem(
+        id="https://globalise.huygens.knaw.nl",
+        label="GLOBALISE Project",
+        homepage=[
+            iiif_prezi3.HomepageItem(
+                id="https://globalise.huygens.knaw.nl",
+                type="Text",
+                format="text/html",
+                label="GLOBALISE Project",
+            )
+        ],
+        logo=[logo],
+    )
+    manifest.provider = [provider]
+
+    manifest.create_thumbnail_from_iiif(
+        "https://service.archief.nl/iipsrv?IIIF=/8e/8c/4d/8e/c4/05/4e/ff/a3/39/6b/55/ba/55/bc/95/75fbe126-7735-410e-ba4f-4f4a7a9a06e7.jp2"
     )
 
     for i in df.itertuples():
-
         index = i.index
 
         iiif_info_url = i.iiif_info_url
@@ -46,19 +90,25 @@ def make_manifest(df):
 
 
 def main(selection_filepath="selection.csv", output_filepath="manifest.json"):
-
     df = pd.read_csv(selection_filepath)
 
-    manifest = make_manifest(df)
+    manifest = make_manifest(df, navdate="1781-01-01T00:00:00Z")
 
-    manifest_jsonld = manifest.jsonld_dict()
-    with open(output_filepath, "w") as outfile:
-        json.dump(manifest_jsonld, outfile, indent=2)
+    with open(output_filepath, "w", encoding="utf-8") as f:
+        s = manifest.json(indent=2)
+
+        js = json.loads(s)
+
+        js["@context"] = [
+            "http://iiif.io/api/extension/navplace/context.json",
+            "http://iiif.io/api/presentation/3/context.json",
+        ]
+
+        json.dump(js, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
-
     main(
-        selection_filepath="data/selection.csv",
+        selection_filepath="static/data/selection.csv",
         output_filepath="static/iiif/manifest.json",
     )
