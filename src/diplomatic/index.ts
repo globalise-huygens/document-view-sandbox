ecf54816806bbe9b278f72c591935ae4fe7079ee
Bas Leenknegt <basleenknegt@gmail.com>
2025-11-26 20:07:48 +0100

    WIP. Convert diplomatic js into ts, remove dead code

--- src/diplomatic/index.ts
+++ src/diplomatic/index.ts
@@ -1,20 +1,17 @@
-import { parseXml } from "@rgrove/parse-xml";
+import {parseXml, XmlElement, XmlText} from "@rgrove/parse-xml";
 
-import { select } from "d3-selection";
-import { polygonHull } from "d3-polygon";
-import { line, curveLinearClosed } from "d3-shape";
-import { pointsOnPath } from "points-on-path";
+import {select} from "d3-selection";
+import {polygonHull} from "d3-polygon";
+import {line, curveLinearClosed} from "d3-shape";
 
 var svgbody = select("body")
   .append("svg")
   .attr("id", "svgbody")
   .attr("width", 4000)
   .attr("height", 5500);
-//.attr("viewbox", "0 0 20 10")
 
 const boundingBox = (points) => {
   // find the x and y bounds from a set of points
-
   let max_x = null;
   let min_x = null;
   let max_y = null;
@@ -27,8 +24,8 @@ const boundingBox = (points) => {
     max_x = x > max_x ? x : max_x;
     max_y = y > max_y ? y : max_y;
 
-    min_x = min_x == null || x < min_x ? x : min_x;
-    min_y = min_y == null || y < min_y ? y : min_y;
+    min_x = min_x === null || x < min_x ? x : min_x;
+    min_y = min_y === null || y < min_y ? y : min_y;
   });
 
   return {
@@ -40,9 +37,6 @@ const boundingBox = (points) => {
     height: max_y - min_y,
   };
 };
-
-const distance = (a, b) =>
-  Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
 
 const radius = (a, b) => Math.atan2(b[1] - a[1], b[0] - a[0]);
 
@@ -63,8 +57,11 @@ const highest_segments = (points) => {
 
   // add the previous point. add the last if it's not there
 
-  if (highest_index > 0) seg.push(points[highest_index - 1]);
-  else seg.push(points[points.length - 1]);
+  if (highest_index > 0) {
+    seg.push(points[highest_index - 1]);
+  } else {
+    seg.push(points[points.length - 1]);
+  }
 
   // add the highest point
 
@@ -72,15 +69,17 @@ const highest_segments = (points) => {
 
   // if the next point is beyond the length, add the first point
 
-  if (points.length > highest_index + 1) seg.push(points[highest_index + 1]);
-  else seg.push(points[0]);
+  if (points.length > highest_index + 1) {
+    seg.push(points[highest_index + 1]);
+  } else {
+    seg.push(points[0]);
+  }
 
   return seg;
 };
 
 const renderWord = (text, coords, container) => {
-  let scale = 1; //0.3
-  //document.getElementById("svgbody").style.transform = "scale(0.3)"
+  let scale = 1;
 
   const path = "M" + coords + "Z";
 
@@ -113,7 +112,7 @@ const renderWord = (text, coords, container) => {
   container.appendChild(c);
 
   c.style.position = "absolute";
-  //c.style.border = "dashed 1px blue"
+  c.style.border = "dashed 1px blue"
 
   c.style.left = bbox.x * scale + "px";
   c.style.top = bbox.y * scale + "px";
@@ -155,14 +154,7 @@ const renderWord = (text, coords, container) => {
 
 // https://dev.to/jankapunkt/make-text-fit-it-s-parent-size-using-javascript-m40
 
-//const isOverflown = ({ clientHeight, scrollHeight }) => scrollHeight > clientHeight
-const isOverflown = ({
-  clientWidth,
-  clientHeight,
-  scrollWidth,
-  scrollHeight,
-}) => scrollWidth > clientWidth;
-//const isOverflown = ({ clientWidth, clientHeight, scrollWidth, scrollHeight }) => (scrollWidth > clientWidth) || (scrollHeight > clientHeight)
+const isOverflown = ({clientWidth, scrollWidth}) => scrollWidth > clientWidth;
 
 const resizeText = (el, minSize = 1, maxSize = 150, step = 1, unit = "px") => {
   {
@@ -175,7 +167,9 @@ const resizeText = (el, minSize = 1, maxSize = 150, step = 1, unit = "px") => {
       el.style.fontSize = `${i}${unit}`;
       overflow = isOverflown(parent);
 
-      if (!overflow) i += step;
+      if (!overflow) {
+        i += step;
+      }
     }
 
     // revert to last state where no overflow happened
@@ -190,9 +184,9 @@ const resizeText = (el, minSize = 1, maxSize = 150, step = 1, unit = "px") => {
 
 const observer = new MutationObserver((mutationList, observer) => {
   for (const mut of mutationList) {
-    if (mut.type == "childList") {
+    if (mut.type === "childList") {
       for (const ad of mut.addedNodes) {
-        if (ad.className == "text") {
+        if (ad instanceof HTMLElement && ad.className === "text") {
           resizeText(ad);
           // TODO: remember the font weight and use it as a starting point for the next
         }
@@ -210,29 +204,55 @@ observer.observe(container, {
   subtree: true,
 });
 
+export const isXmlElement = (el: any): el is XmlElement =>
+  el instanceof XmlElement;
+export function assertXmlElement(el: unknown): asserts el is XmlElement {
+  if(!isXmlElement(el)) throw new Error("Expected XmlElement");
+}
+export const isXmlText = (el: any): el is XmlText =>
+  el instanceof XmlText;
+export function assertXmlText(el: unknown): asserts el is XmlText {
+  if(!isXmlText(el)) throw new Error("Expected XmlText");
+}
+
 fetch("/data/3598_selection/NL-HaNA_1.04.02_3598_0797.xml")
   .then((response) => response.text())
-  .then((data) => {
-    const xml = parseXml(data);
-
-    const regions = xml.children[0].children
-      .filter((x) => x["name"] == "Page")[0]
-      .children.filter((x) => x["name"] == "TextRegion");
+  .then((body) => {
+    const xml = parseXml(body);
+    const doc = xml.children[0];
+    if(!isXmlElement(doc)) throw new Error("Expected XmlElement")
+    const firstPage = doc.children
+      .filter((x) => {
+        return x["name"] === "Page";
+      })[0];
+    if(!isXmlElement(firstPage)) throw new Error("Expected XmlElement")
+    const regions = firstPage
+      .children.filter((x) => x["name"] === "TextRegion");
 
     for (const region of regions) {
-      const lines = region.children.filter((x) => x["name"] == "TextLine");
-
+      assertXmlElement(region);
+      const lines = region.children
+        .filter((x) => x["name"] === "TextLine");
       for (const line of lines) {
-        const words = line.children.filter((x) => x["name"] == "Word");
+        assertXmlElement(line);
+        const words = line.children
+          .filter((x) => x["name"] === "Word");
         for (const word of words) {
-          const coords = word.children.filter((x) => x["name"] == "Coords")[0]
-            .attributes["points"];
-
-          const text = word.children
-            .filter((x) => x["name"] == "TextEquiv")[0]
-            .children.filter((x) => x["name"] == "Unicode")[0].children[0].text;
-
-          renderWord(text, coords, container);
+          assertXmlElement(word);
+          const coords = word.children
+            .find((x) => x["name"] === "Coords");
+          assertXmlElement(coords);
+          const points = coords.attributes["points"];
+          const textEquiv = word.children
+            .find((x) => x["name"] === "TextEquiv");
+          assertXmlElement(textEquiv);
+          const unicode = textEquiv.children
+            .find((x) => x["name"] === "Unicode");
+          assertXmlElement(unicode);
+          const textChild = unicode.children[0];
+          assertXmlText(textChild);
+          const text = textChild.text;
+          renderWord(text, points, container);
         }
       }
     }
