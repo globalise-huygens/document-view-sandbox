a3efdbcc630c603e1f7de0de0c0439f2e2758396
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-04 14:35:53 +0100

    WIP. Remember last font size with text resizer

--- src/diplomatic/TextResizer.ts
+++ src/diplomatic/TextResizer.ts
@@ -4,30 +4,53 @@ import {debounce} from "lodash";
  * Inspiration: https://dev.to/jankapunkt/make-text-fit-it-s-parent-size-using-javascript-m40
  */
 export class TextResizer {
+  private prevFontsize: number | null = null;
   private isOverflownCount = 0
 
   public resize = (el: HTMLElement, minSize = 1, maxSize = 150, step = 1) => {
     let i = minSize;
     let overflow = false;
-
+    let direction: 'asc' | 'desc' = 'asc';
     const parent = el.parentNode as HTMLElement;
-
-    while (!overflow && i < maxSize) {
+    if(this.prevFontsize) {
+      i = this.prevFontsize
       el.style.fontSize = `${i}px`;
-      overflow = this.isOverflown(parent);
-
-      if (!overflow) {
-        i += step;
+      overflow = this.isOverflown(parent)
+      if (overflow) {
+        direction = 'desc'
       }
     }
 
+    let finalFontSize;
+    if (direction === 'desc') {
+      while (overflow && i > 0) {
+        el.style.fontSize = `${i}px`;
+        overflow = this.isOverflown(parent);
+        if (overflow) {
+          i -= step;
+        }
+      }
+      finalFontSize = i;
+    } else {
+      while (!overflow && i < maxSize) {
+        el.style.fontSize = `${i}px`;
+        overflow = this.isOverflown(parent);
+        if (!overflow) {
+          i += step;
+        }
+        this.isOverflownCount++
+      }
+      finalFontSize = i - step;
+    }
+
     // revert to last state where no overflow happened
-    el.style.fontSize = `${i - step}px`;
+    el.style.fontSize = `${finalFontSize}px`;
 
     // adjust the vertical positioning after the horizontal scaling of the font.
     const verticalAdjust = parent.clientHeight / 2 - el.clientHeight / 2;
     el.style.marginTop = `${verticalAdjust}px`;
 
+    this.prevFontsize = finalFontSize;
   };
 
   private isOverflown = ({clientWidth, scrollWidth}) => {
