d14d809688ead8092f8028f44f3f0b7ba493c809
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-09 12:26:27 +0100

    Split calibrating and resizing

--- src/diplomatic/renderText.ts
+++ src/diplomatic/renderText.ts
@@ -15,33 +15,45 @@ export function renderText(
   const resizeTextBench = new Benchmark(TextResizer.name);
   const resizer = new TextResizer();
   const regions = page.children.filter((x) => x["name"] === "TextRegion");
-  for (const region of regions) {
-    assertXmlElement(region);
-    const lines = region.children
-      .filter((x) => x["name"] === "TextLine");
-    for (const line of lines) {
-      assertXmlElement(line);
-      const words = line.children
-        .filter((x) => x["name"] === "Word");
-      for (const word of words) {
-        assertXmlElement(word);
-        const coords = word.children
-          .find((x) => x["name"] === "Coords");
-        assertXmlElement(coords);
-        const points = coords.attributes["points"];
-        const textEquiv = word.children
-          .find((x) => x["name"] === "TextEquiv");
-        assertXmlElement(textEquiv);
-        const unicode = textEquiv.children
-          .find((x) => x["name"] === "Unicode");
-        assertXmlElement(unicode);
-        const textChild = unicode.children[0];
-        assertXmlText(textChild);
-        const text = textChild.text;
-        const $word = renderWord(text, points, $text, $boundaries, scale);
-        resizeTextBench.run(() => resizer.resize($word));
+
+  const $words = findWords();
+
+  resizeTextBench.run(() => {
+    resizer.calibrate($words.slice(0, 10))
+    $words.forEach(word => resizer.resize(word))
+  })
+
+  function findWords(): HTMLDivElement[] {
+    const $found = []
+    for (const region of regions) {
+      assertXmlElement(region);
+      const lines = region.children
+        .filter((x) => x["name"] === "TextLine");
+      for (const line of lines) {
+        assertXmlElement(line);
+        const words = line.children
+          .filter((x) => x["name"] === "Word");
+        for (const word of words) {
+          assertXmlElement(word);
+          const coords = word.children
+            .find((x) => x["name"] === "Coords");
+          assertXmlElement(coords);
+          const points = coords.attributes["points"];
+          const textEquiv = word.children
+            .find((x) => x["name"] === "TextEquiv");
+          assertXmlElement(textEquiv);
+          const unicode = textEquiv.children
+            .find((x) => x["name"] === "Unicode");
+          assertXmlElement(unicode);
+          const textChild = unicode.children[0];
+          assertXmlText(textChild);
+          const text = textChild.text;
+          const $word = renderWord(text, points, $text, $boundaries, scale);
+          $found.push($word)
+        }
       }
     }
+    return $found
   }
 }
 
