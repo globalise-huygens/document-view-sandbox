031f449737a184191cba62ed0071bb733649aa08
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-09 12:26:27 +0100

    Split calibrating and resizing

--- src/diplomatic/TextResizer.ts
+++ src/diplomatic/TextResizer.ts
@@ -10,26 +10,47 @@ export class TextResizer {
 
   constructor(
     private precision = 3,
-    private charToWidthFactor = 0.6
+    private charToWidthFactor = 1
   ) {
+  }
+
+  public calibrate(elements: HTMLElement[]): void {
+    for (const element of elements) {
+      const parent = element.parentNode as HTMLElement;
+      const charCount = element.textContent?.length || 0;
+      if (!charCount) {
+        continue
+      }
+      const width = parent.clientWidth;
+      const predicted = this.calcWidth(width, charCount);
+      const finalSize = this.binarySearch(element, parent, predicted);
+      this.updateFactor(width, charCount, finalSize);
+    }
+    console.log('Calibrated, new char-to-width factor:', this.charToWidthFactor)
   }
 
   public resize = (el: HTMLElement) => {
     const parent = el.parentNode as HTMLElement;
-    const charCount = el.textContent?.length || 0
-    if(!charCount) return;
+    const charCount = el.textContent?.length
+    if (!charCount) return;
     const width = parent.clientWidth;
 
     const predicted = this.calcWidth(width, charCount);
-    let low = predicted * 0.9;
-    let high = predicted * 1.1;
-
     el.style.fontSize = `${predicted}px`;
-    if (this.isOverflown(parent)) {
-      high = predicted - 1;
-    } else {
-      low = predicted + 1;
-    }
+
+    // adjust the vertical positioning after the horizontal scaling of the font.
+    const verticalAdjust = parent.clientHeight / 2 - el.clientHeight / 2;
+    el.style.marginTop = `${verticalAdjust}px`;
+  }
+
+  private binarySearch(
+    el: HTMLElement,
+    parent: HTMLElement,
+    predicted: number
+  ): number {
+    el.style.fontSize = `${predicted}px`;
+    let low = predicted * 0.5;
+    let high = predicted * 1.5;
 
     let mid = low;
     while (low <= high) {
@@ -49,14 +70,10 @@ export class TextResizer {
     }
 
     el.style.fontSize = `${mid}px`;
-    this.calcFactor(width, charCount, mid);
+    return mid;
+  }
 
-    // adjust the vertical positioning after the horizontal scaling of the font.
-    const verticalAdjust = parent.clientHeight / 2 - el.clientHeight / 2;
-    el.style.marginTop = `${verticalAdjust}px`;
-  };
-
-  private calcFactor(
+  private updateFactor(
     width: number,
     charCount: number,
     finalFontSize: number
