56c7d29114f9bbf0530e61d8d89f17e6ce78e614
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-11 14:36:47 +0100

    WIP. Implement renderAnnoText

---
+++ src/diplomatic/anno/assertSvgSelector.ts
@@ -0,0 +1,10 @@
+import { Selector } from "../AnnoModel";
+import { isSvgSelector } from "./isSvgSelector";
+
+export function assertSvgSelector(
+  selector: unknown
+): asserts selector is Selector {
+  if (!isSvgSelector(selector)) {
+    throw new Error("Expected SvgSelector");
+  }
+}
---
+++ src/diplomatic/anno/findAnnoWords.ts
@@ -0,0 +1,32 @@
+import { IiifAnnotationPage } from "../AnnoModel";
+import { assertTextualBody } from "./assertTextualBody";
+import { assertSpecificResourceTarget } from "./assertSpecificResourceTarget";
+import { assertSvgSelector } from "./assertSvgSelector";
+import { parseSvgPath } from "./parseSvgPath";
+
+export function findAnnoWords(
+  annoPage: IiifAnnotationPage
+): { text: string; points: string }[] {
+  const words = [];
+  for (const annotation of annoPage.items) {
+    if (annotation.textGranularity !== "word") {
+      continue;
+    }
+    if (!annotation.body || annotation.body.length === 0) {
+      throw new Error("Annotation missing body");
+    }
+    const body = annotation.body[0];
+    assertTextualBody(body);
+    const text = body.value;
+    if (!annotation.target || annotation.target.length === 0) {
+      throw new Error("Annotation missing target");
+    }
+    const target = annotation.target[0];
+    assertSpecificResourceTarget(target);
+    const selector = target.selector;
+    assertSvgSelector(selector);
+    const points = parseSvgPath(selector.value);
+    words.push({ text, points });
+  }
+  return words;
+}
