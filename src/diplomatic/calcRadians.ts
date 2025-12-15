fec0f2ea8ab855eb9d83ab3c9ff6efa4d42921dd
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-15 13:34:30 +0100

    Refactor and document text angle calculations

--- src/diplomatic/calcRadians.ts
+++ src/diplomatic/calcRadians.ts
@@ -1,4 +1,4 @@
 import { Point } from "./Point";
 
-export const calcRadius = (a: Point, b: Point) =>
+export const calcRadians = (a: Point, b: Point) =>
   Math.atan2(b[1] - a[1], b[0] - a[0]);
--- src/diplomatic/renderWord.ts
+++ src/diplomatic/renderWord.ts
@@ -1,13 +1,13 @@
 import {Point} from "./Point";
-import {calcRadius} from "./calcRadius";
 import {calcBoundingBox} from "./calcBoundingBox";
-import {findHighestSegments} from "./findHighestSegments";
+import {calcBaseSegment} from "./calcBaseSegment";
 import {polygonHull} from "d3-polygon";
+import {calcTextAngle} from "./calcTextAngle";
 
 export type Word = {
   el: HTMLDivElement,
   hull: Point[],
-  segment: Point[]
+  base: Point[]
 }
 
 export const renderWord = (
@@ -23,19 +23,10 @@ export const renderWord = (
   }
 
   const hull = polygonHull(coordarr);
+  const base = calcBaseSegment(hull);
+  const angle = calcTextAngle(base);
 
-  // get the two segments that connect the 'highest' (lowest) point
-  const segment = findHighestSegments(hull);
-
-  // calculate the angles of the segments and select the 'smallest'
-  // TODO: is this indeed the best segment?
-
-  const seg_rad1 = calcRadius(segment[0], segment[1]);
-  const seg_rad2 = calcRadius(segment[1], segment[2]);
-  const seg_rad = Math.abs(seg_rad1) < Math.abs(seg_rad2) ? seg_rad1 : seg_rad2;
-
-  // TODO: first, rotate all points and then caclulate the box
-
+  // TODO: first, rotate all points and then calculate the box
   const boundingBox = calcBoundingBox(hull);
   const $boundingBox = document.createElement("div");
   $text.appendChild($boundingBox);
@@ -44,7 +35,7 @@ export const renderWord = (
   $boundingBox.style.top = boundingBox.y * scale + "px";
   $boundingBox.style.width = boundingBox.width * scale + "px";
   $boundingBox.style.height = boundingBox.height * scale + "px";
-  $boundingBox.style.transform = "rotate(" + seg_rad + "rad)";
+  $boundingBox.style.transform = "rotate(" + angle + "rad)";
 
   const $wordText = document.createElement("div");
   $boundingBox.appendChild($wordText);
@@ -53,6 +44,6 @@ export const renderWord = (
   $wordText.style.whiteSpace = "nowrap";
   $wordText.style.display = "block";
   $wordText.style.fontSize = "8px";
-  return {el: $wordText, hull, segment};
+  return {el: $wordText, hull, base};
 };
 
