b369d13c7f107a0987737d1d4044c299365e6636
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-15 16:09:44 +0100

    Use and extend @iiif/presentation-3 types

--- src/diplomatic/anno/findWordAnnotations.ts
+++ src/diplomatic/anno/findWordAnnotations.ts
@@ -1,32 +1,43 @@
-import { IiifAnnotationPage } from "../AnnoModel";
-import { assertTextualBody } from "./assertTextualBody";
-import { assertSpecificResourceTarget } from "./assertSpecificResourceTarget";
-import { assertSvgSelector } from "./assertSvgSelector";
-import { parseSvgPath } from "./parseSvgPath";
+// src/diplomatic/anno/findWordAnnotations.ts
+import type {IiifAnnotationPage} from '../AnnoModel';
+import {assertTextualBody} from './assertTextualBody';
+import {assertSpecificResourceTarget} from './assertSpecificResourceTarget';
+import {assertSvgSelector} from './assertSvgSelector';
+import {parseSvgPath} from './parseSvgPath';
+import {isEmpty} from "lodash";
+
+export type WordAnnotation = { text: string; points: string };
 
 export function findWordAnnotations(
   annoPage: IiifAnnotationPage,
-): { text: string; points: string }[] {
-  const words = [];
+): WordAnnotation[] {
+  const words: WordAnnotation[] = [];
+  if (!annoPage.items) {
+    return words;
+  }
   for (const annotation of annoPage.items) {
-    if (annotation.textGranularity !== "word") {
+    if (annotation.textGranularity !== 'word') {
       continue;
     }
-    if (!annotation.body || annotation.body.length === 0) {
-      throw new Error("Annotation missing body");
+    if (!annotation.body || isEmpty(annotation.body)) {
+      throw new Error('Annotation missing body');
     }
-    const body = annotation.body[0];
+    const body = Array.isArray(annotation.body)
+      ? annotation.body[0]
+      : annotation.body;
     assertTextualBody(body);
     const text = body.value;
-    if (!annotation.target || annotation.target.length === 0) {
-      throw new Error("Annotation missing target");
+    if (!annotation.target || isEmpty(annotation.target)) {
+      throw new Error('Annotation missing target');
     }
-    const target = annotation.target[0];
+    const target = Array.isArray(annotation.target)
+      ? annotation.target[0]
+      : annotation.target;
     assertSpecificResourceTarget(target);
     const selector = target.selector;
     assertSvgSelector(selector);
     const points = parseSvgPath(selector.value);
-    words.push({ text, points });
+    words.push({text, points});
   }
   return words;
 }
