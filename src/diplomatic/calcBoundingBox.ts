2a21c9dbff70757046880c9cbbba0db55547570c
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-15 15:08:01 +0100

    Clean up calcBoundingBox

--- src/diplomatic/calcBoundingBox.ts
+++ src/diplomatic/calcBoundingBox.ts
@@ -4,28 +4,24 @@ import { Point } from "./Point";
  * find the x and y bounds from a set of points
  */
 export const calcBoundingBox = (points: Point[]) => {
-  let max_x = null;
-  let min_x = null;
-  let max_y = null;
-  let min_y = null;
+  let xMin = points[0][0];
+  let yMin = points[0][1];
+  let xMax = points[0][0];
+  let yMax = points[0][1];
 
-  points.forEach((p) => {
-    const x = p[0];
-    const y = p[1];
-
-    max_x = x > max_x ? x : max_x;
-    max_y = y > max_y ? y : max_y;
-
-    min_x = min_x === null || x < min_x ? x : min_x;
-    min_y = min_y === null || y < min_y ? y : min_y;
-  });
+  for (let i = 1; i < points.length; i++) {
+    const x = points[i][0];
+    const y = points[i][1];
+    xMin = Math.min(xMin, x);
+    xMax = Math.max(xMax, x);
+    yMin = Math.min(yMin, y);
+    yMax = Math.max(yMax, y);
+  }
 
   return {
-    x: min_x,
-    y: min_y,
-    x2: max_x,
-    y2: max_y,
-    width: max_x - min_x,
-    height: max_y - min_y,
+    x: xMin,
+    y: yMin,
+    width: xMax - xMin,
+    height: yMax - yMin,
   };
 };
