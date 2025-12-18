636c15f76511f047ed163b6854ea4dcdea5a41af
Bas Leenknegt <basleenknegt@gmail.com>
2025-12-18 11:16:24 +0100

    Scale view using slider

---
+++ src/diplomatic/example/renderTextOnlyExample.ts
@@ -0,0 +1,45 @@
+import {IiifAnnotationPage} from "../AnnoModel";
+import {px} from "../px";
+import {renderDiplomaticView} from "../renderDiplomaticView";
+import {$} from "./$";
+
+export async function renderTextOnlyExample(
+  $parent: HTMLElement
+) {
+
+  const jsonPath = '../iiif/annotations/transcriptions/NL-HaNA_1.04.02_3598_0797.json';
+
+  $parent.classList.add('text-only')
+  $parent.innerHTML = `<div class="diplomatic-view"></div>`
+  const $menu = $('#menu')
+  const $view: HTMLDivElement = $('.diplomatic-view', $parent);
+  const $slider = document.createElement('span')
+  $menu.appendChild($slider)
+  $slider.classList.add('slider')
+  $slider.innerHTML = `
+    zoom: min 
+    <input type="range" value="40" min="5" max="100"/> 
+    max`
+  const $input: HTMLInputElement = $('input', $slider)
+
+  const annoResponse = await fetch(jsonPath);
+  const annoPage: IiifAnnotationPage = await annoResponse.json();
+
+  const {width: parentWidth} = $parent.getBoundingClientRect();
+  const {width, height} = annoPage.partOf;
+
+  let sliderScale = -1
+  const adjustScale = () => {
+    sliderScale = parseInt($input.value) / 100;
+    const scale = Math.max(parentWidth / +width) * sliderScale;
+    $view.style.height = px(scale * height);
+    $view.style.width = px(scale * width);
+
+    const viewConfig = {showBoundaries: true, showScanMargin: false};
+    renderDiplomaticView($view, annoPage, viewConfig);
+    console.log('scale', sliderScale)
+  }
+
+  $slider.addEventListener('change', adjustScale);
+  adjustScale();
+}
