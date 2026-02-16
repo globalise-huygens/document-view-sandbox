6807ffe9cfc919ada100143b530b5583bcdc4b04
Bas Leenknegt <basleenknegt@gmail.com>
2026-02-16 14:49:00 +0100

    Remove original layout example

--- src/diplomatic/react/example/Example.tsx
+++ src/diplomatic/react/example/Example.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {OriginalLayoutExample} from "./OriginalLayoutExample";
 import {$} from "../../example/$";
 import {getExampleFromUrl} from "../../../util/getExampleFromUrl";
 import {DualViewExample} from "./DualViewExample";
@@ -9,9 +8,7 @@ const examples = ['original-layout', 'dual-view'] as const;
 export function Example() {
   const example = getExampleFromUrl(examples);
   $(`a.${example}`)?.classList.add('active');
-  if (example === 'original-layout') {
-    return <OriginalLayoutExample/>;
-  } else if (example === 'dual-view') {
+  if (example === 'dual-view') {
     return <DualViewExample/>;
   } else {
     return "Unknown example"
