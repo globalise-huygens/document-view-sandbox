6e7ff2e9058594852547f842010e81e23eef605c
Bas Leenknegt <basleenknegt@gmail.com>
2026-02-16 15:21:54 +0100

    Remove original layout from react example list

--- src/diplomatic/react/example/Example.tsx
+++ src/diplomatic/react/example/Example.tsx
@@ -3,7 +3,7 @@ import {$} from "../../example/$";
 import {getExampleFromUrl} from "../../../util/getExampleFromUrl";
 import {DualViewExample} from "./DualViewExample";
 
-const examples = ['original-layout', 'dual-view'] as const;
+const examples = ['dual-view'] as const;
 
 export function Example() {
   const example = getExampleFromUrl(examples);
